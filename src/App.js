ea49b03342d12a22db42d76528cbdd719c4912e8
heidenreichgeorg <georg.heidenreich@bexar.de>
2024-01-11 11:13:21 +0100

    load from array

--- src/App.js
+++ src/App.js
@@ -1,65 +1,68 @@
-import logo from '../public/favicon.bmp';
+import logo from './favicon.bmp';
 import './App.css';
+
+let currentMenu = [
+    "BI_Aufsesser_Dunkel.png",
+    "BI_Schinner_Edelpils.png",
+    "BI_Steam_Porter.png",
+    "BI_Steam_SummerAle.png",
+    "BI_Veldensteiner_Landbier.png",
+    "BI_Weller_JeanPaul.png",
+    "BI_Aktien_Landbier_Dunkel.png",
+    "BI_Rittmayer_Hallerndorfer_Hausbrauer.png",
+
+    "RW_Rioja_Faustino_2019.png",
+    "RW_Teroldego_2014.png",
+    "RW_ZehnthofFeinherb_2019.png"
+];
+
+function splitChoice(fileName) {
+    let aParts= (fileName.split('.')[0]).split('_');
+    aParts.shift();
+    return aParts.join(' ');
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
+    <div className="paper">
+        <header className="App-header">
+            <img src={logo}  className="App-logo" alt="logo" />
+        </header>
+                
 
-<div class="menu">
-    <div class="caption">
-    </div><div class="title">
-        <div class="choice">Biere</div>
-    </div>
-    <div class="course">
-        <div class="spacer"><img width="10px"  src="./paper.png"></img></div>
-        <div class="choice">
-            <div class="choice">
-                    <div class="course"><img width="80px" height="200px" src="./B_Aufsesser_Dunkel.png"></img></div>
-                    <div class="course">Aufsesser Dunkel</div>
+        <div class="menu">
+            <div class="title">
+                <div class="choice">Biere</div>
+            </div>
+
+            <div class="course">
+                <div class="spacer"><img width="10px"  src="./paper.png"></img></div>
+                {
+                currentMenu.map((item,i)=>(item.substring(0,2)=="BI"? (
+                    <div class="choice">
+                            <div class="course"><img width="80px" alt="{item}" height="200px" src={"./"+item}></img></div>
+                            <div class="course">{splitChoice(item)}</div>
+                    </div>
+                ):""))
+                }       
+            </div>
+            
+            <div class="title">
+                <div class="choice">Rotweine</div>
+            </div>
+
+            <div class="course">
+                <div class="spacer"><img width="10px"  src="./paper.png"></img></div>
+                {
+                currentMenu.map((item,i)=>(item.substring(0,2)=="RW"? (
+                    <div class="choice"><div class="menu">
+                            <div class="course"><img width="80px" alt="{item}" height="200px" src={"./"+item}></img></div>
+                            <div class="course">{splitChoice(item)}</div>
+                    </div></div>
+               ):""))
+                }       
             </div>
         </div>
-        <div class="choice"><img width="80px" height="200px"  src="./B_Schinner_Edelpils.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./B_Steam_Porter.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./B_Steam_SummerAle.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./B_Veldensteiner_Landbier.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./B_Weller_JeanPaul.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./B_Aktien_Landbier_Dunkel.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./B_Rittmayer_Hallerndorfer_Hausbrauer.png"></img></div>            
-    </div>
-    
-    <div class="caption">
-        <div class="spacer"><img width="10px"  src="./paper.png"></img></div>
-        <div class="choice">Aufsesser Dunkel</div>
-        <div class="choice">Schinner Edelpils</div>
-        <div class="choice">Steam Porter</div>
-        <div class="choice">Steam SummerAle</div>
-        <div class="choice">Veldensteiner Landbier</div>
-        <div class="choice">Weller Jean-Paul</div>
-        <div class="choice">Aktien Landbier Dunkel</div>
-        <div class="choice">Rittmayer Hallerndorfer Hausbrauer</div>            
-    </div>
-
-    <div class="title">
-        <div class="choice">Rotweine</div>
-    </div><div class="course">
-        <div class="spacer"><img width="10px"  src="./paper.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./RW_Rioja_Faustino_2019.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./RW_Teroldego_2014.png"></img></div>
-        <div class="choice"><img width="80px" height="200px"  src="./RW_ZehnthofFeinherb_2019.png"></img></div>
-    </div>
-
-    <div class="caption">
-        <div class="spacer"><img width="10px"  src="./paper.png"></img></div>
-        <div class="choice">Rioja Faustino 2019</div>
-        <div class="choice">Teroldego Gaudenzio 2014</div>
-        <div class="choice">Zehnthof Feinherb 2019</div>
-    </div>
-</div>
-       
-      </header>
     </div>
   );
 }
