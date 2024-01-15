5350c184241a40c853e5edf1650549f33cae0eeb
heidenreichgeorg <georg.heidenreich@bexar.de>
2024-01-15 18:56:45 +0100

    generic course generator

--- src/App.js
+++ src/App.js
@@ -4,32 +4,56 @@ import './App.css';
 import { useSearchParams } from 'react-router-dom';
 
 // created workspace with      npx create-react-app eg_menu
-// added router with npm install react-router-dom
+// added router with         npm install react-router-dom
+// created public project in GitHub
 
 let basic = [
-    "BI_Aufsesser_Dunkel.png",
-    "BI_Rittmayer_Hallerndorfer_Hausbrauer.png",
     "BI_Schinner_Edelpils.png",
-    "BI_Weller_JeanPaul.png",
+    "BI_Aufsesser_Dunkel.png", 
+    "BI_Rittmayer_Hallerndorfer_Hausbrauer.png",
     "RW_Rioja_Faustino_2019.png"
 ];
 
-
-
 let events = {
-    "240117": [
+    "1": [
+        "BI_Steam_Porter.png",
+    ],
+
+    "2": [
         "BI_Aktien_Landbier_Dunkel.png",
         "BI_Aktien_Zwickl_Kellerbier.png",
-        "BI_Steam_Porter.png",
         "BI_Steam_SummerAle.png",
-        "BI_Veldensteiner_Landbier.png",
+    ],
 
-        "RW_Teroldego_2014.png",
-        "RW_Zehnthof_Feinherb_2019.png",
+    "3": [
+        "BI_Veldensteiner_Landbier.png"
+    ],
 
-        "MV_Veganes_Linsen_Dal.png"
-        ]
+    "240117": [
+
+            "BI_Rittmayer_Hallerndorfer_Kellerbier.png",
+        
+            "RW_Teroldego_2014.png",
+            "RW_Zehnthof_Feinherb_2019.png",
+
+            "MV_Veganes_Linsen_Dal.png"
+        ],
+
+    "240118": [
+
+            "BI_Weller_JeanPaul.png",
+        
+            "SW_Mars_Celebrations.png",
+
+            "MV_Chili_sin_Carne.png"
+            ]
+    
 };
+
+
+let reserve = [
+           
+]
 
 
 function splitChoice(fileName) {
@@ -45,14 +69,17 @@ function App() {
     let currentMenu=basic;
     if(date && events[date] &&  events[date].length>0) try {
 
-        //events[date].forEach((choice)=>{currentMenu.push(choice)})
         currentMenu=events[date].concat(currentMenu);
 
     } catch(e) { console.dir("could not add event list")}
 
-    let listBeer = [];  currentMenu.forEach((item,i)=>{if(item.substring(0,2)==="BI") listBeer.push(item); })
-    let listRedWine=[]; currentMenu.forEach((item,i)=>{if(item.substring(0,2)==="RW") listRedWine.push(item); })
-    let listMainVeg=[]; currentMenu.forEach((item,i)=>{if(item.substring(0,2)==="MV") listMainVeg.push(item); })
+    let jCourses = {"BI":"Bier", "RW":"Rotwein", "MV":"Veganes Hauptgericht", "SW":"Süßigkeiten"};
+
+    let jChoices={};
+    currentMenu.forEach((item,i)=>{
+        let type=item.substring(0,2);
+        if(jChoices[type]) jChoices[type].push(item); else jChoices[type]=[item];
+    });
 
     return (
         <div className="paper">
@@ -66,73 +93,29 @@ function App() {
 
             
 
-            {(listRedWine.length>0) ?
+            {   Object.keys(jChoices).map((type)=>(
+                (jChoices[type].length>0) ?
                 (
                     <div>
                         <div className="title">
-                            <div className="choice">Rotwein</div>
+                            <div className="choice">{jCourses[type]}</div>
                         </div>
                         <div className="course">
                             <div className="spacer"><img  alt="" width="10px"  src="./paper.png"></img></div>
                             {
-                            listRedWine.map((item,i)=>
+                            jChoices[type].map((item,i)=>
                             (
                                 <div key={"redwine"+i} className="choice"><div className="menu">
-                                        <div className="course"><img alt="redwine" width="80px"  height="200px" src={"./"+item}></img></div>
+                                        <div className="course"><img alt={'"'+type+'"'} width="80px"  height="200px" src={"./"+item}></img></div>
                                         <div className="course">{splitChoice(item)}</div>
                                 </div></div>
                             ))
                             }      
                         </div>
                     </div>
-                ):""}
+                ):""))
+            }
 
-
-
-                {(listBeer.length>0) ?
-                (
-                    <div>
-                        <div className="title">
-                            <div className="choice">Bier</div>
-                        </div>
-                        <div className="course">
-                            <div className="spacer"><img alt="" width="10px"  src="./paper.png"></img></div>
-                            {
-                            listBeer.map((item,i)=>
-                            (
-                                <div key={"beer"+i} className="choice"><div className="menu">
-                                        <div className="course"><img alt="beer" width="80px" height="200px" src={"./"+item}></img></div>
-                                        <div className="course">{splitChoice(item)}</div>
-                                </div></div>
-                            ))
-                            }      
-                        </div>
-                    </div>
-                ):""}
-
-
-
-
-                {(listMainVeg.length>0) ?
-                (
-                    <div>
-                        <div className="title">
-                            <div className="choice">Hauptgang - vegan</div>
-                        </div>
-                        <div className="course">
-                            <div className="spacer"><img  alt="" width="10px"  src="./paper.png"></img></div>
-                            {
-                            listMainVeg.map((item,i)=>
-                            (
-                                <div key={"vegandish"+i} className="choice"><div className="menu">
-                                        <div className="course"><img alt="main vegan" width="80px" height="200px" src={"./"+item}></img></div>
-                                        <div className="course">{splitChoice(item)}</div>
-                                </div></div>
-                            ))
-                            }      
-                        </div>
-                    </div>
-                ):""}
 
 
             </div>
